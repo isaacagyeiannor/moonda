61950d54813189e82c0840038328a40eaf0b5c40
Isaac Agyei Annor <isaacagyeiannor@gmail.com>
2022-06-23 20:52:02 +0000

    Initial Commit

--- zhonghuaghana/settings.py
+++ zhonghuaghana/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wno9!#^ws0n8lmwz2m9#i60e#-71(efk=%r#2i@27p(7tlscd6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*', 'zhonghuaghana.herokuapp.com', 'www.zhonghuaghana.herokuapp.com']
+ALLOWED_HOSTS = ['*', 'zhonghuaghana.com', 'www.zhonghuaghana.com']
 
 
 # Application definition
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'zhonghuaghana.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'zhonghuaghanadb',
+	'USER': 'busybrain',
+	'PASSWORD': 'Buzy109!',
+	'HOST': 'localhost',
+	'PORT'	: '5432'
     }
 }
 
@@ -123,23 +127,8 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.0/howto/static-files/
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
-# STATIC_URL = 'static/'
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, "mainwebsite/static")]
-# STATIC_ROOT= os.path.join(BASE_DIR,'mainwebsite')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
